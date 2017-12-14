cdaa3a184aa00d33f0e22ab14d91644a25f1c376
LambadaCorez <jackhric@yahoo.com>
2017-12-13 20:40:07 -0500

    FOR SERVER OWNERS: use this for adding things from the workshop

---
+++ Server Files/lua/autorun/server/workshop.lua
@@ -0,0 +1,6 @@
+
+resource.AddWorkshop("123455885")
+resource.AddWorkshop("131243694")
+resource.AddWorkshop("112606459")
+resource.AddWorkshop("1230012354")
+resource.AddFile("materials/layer/race.vmt")
