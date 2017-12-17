628f0f9afeca74d2038995fa9e385dc4e0a0d14b
LambadaCorez <jackhric@yahoo.com>
2017-12-17 14:10:46 -0500

    sdf

--- gamemode/entities/buggies/addon.lua
+++
@@ -1,9 +0,0 @@
-keyvalues = {}
-
-keyvalues[0]={keyvalue="scripts/vehicles/tdmcars/mit_eclipsegsx.txt"}
-keyvalues[1]={keyvalue="scripts/vehicles/tdmcars/nissilvs15.txt"}
-keyvalues[2]={keyvalue="scripts/vehicles/tdmcars/mitsu_evo8.txt"}
-keyvalues[3]={keyvalue="scripts/vehicles/tdmcars/mitsu_evox.txt"}
-keyvalues[4]={keyvalue="scripts/vehicles/tdmcars/skyline_r34.txt"}		
-keyvalues[5]={keyvalue="scripts/vehicles/tdmcars/nis_370z.txt"}
-keyvalues[6]={keyvalue="scripts/vehicles/tdmcars/mit_eclipsegsx.txt"}		
